27874ca4ab0579696526bb11aed16b6f35c3de98
amalkr068 <amalkr068@gmail.com>
2025-03-22 00:36:32 -0700

    new change for doploying

--- corsConfig.js
+++ corsConfig.js
@@ -1,7 +1,7 @@
 const cors = require("cors");
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Vite frontend
+  origin: "melodic-fenglisu-8043ef.netlify.app", // Vite frontend
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
